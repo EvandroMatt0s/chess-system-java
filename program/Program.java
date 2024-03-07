e9123b590883ba7b229b63f7ca710bc8a5a08e87
Evandro Mattos <emattos0114@outlook.com>
2024-03-06 21:39:19 -0300

    first class: Position

--- program/Program.java
+++ program/Program.java
@@ -1,8 +1,12 @@
 package chess_sytem.program;
 
+import chess_sytem.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("hello world!");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+
     }
 }
